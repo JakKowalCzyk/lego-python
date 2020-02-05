966a51e9dc0eb3323adb99e0b6b348532819fe12
Jakub Kowalczyk <beufort@o2.pl>
2020-02-05 19:04:23 +0100

    wwip

--- train.py
+++ train.py
@@ -3,7 +3,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 from keras.preprocessing.image import img_to_array
 from keras.utils import np_utils
-from pipeline.nn.conv import LeNet
 from imutils import paths
 import matplotlib.pyplot as plt
 import numpy as np
