389d2a71cbfd7498d243fb94dea8072afef86ff7
jakub.kowalczyk <jakub.kowalczyk@br-ag.eu>
2020-02-06 11:44:04 +0100

    wip

---
+++ cnn.py
@@ -0,0 +1,34 @@
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.layers.core import Activation, Dropout
+from keras.layers.core import Flatten
+from keras.layers.core import Dense
+from keras import backend as K
+
+class CNN:
+    @staticmethod
+    def build():
+
+        model = Sequential()
+        model.add(Conv2D(filters = 16, kernel_size = 2,input_shape=(100,100,3),padding='same'))
+        model.add(Activation('relu'))
+        model.add(MaxPooling2D(pool_size=2))
+
+        model.add(Conv2D(filters = 32,kernel_size = 2,activation= 'relu',padding='same'))
+        model.add(MaxPooling2D(pool_size=2))
+
+        model.add(Conv2D(filters = 64,kernel_size = 2,activation= 'relu',padding='same'))
+        model.add(MaxPooling2D(pool_size=2))
+
+        model.add(Conv2D(filters = 128,kernel_size = 2,activation= 'relu',padding='same'))
+        model.add(MaxPooling2D(pool_size=2))
+
+        model.add(Dropout(0.3))
+        model.add(Flatten())
+        model.add(Dense(150))
+        model.add(Activation('relu'))
+        model.add(Dropout(0.4))
+        model.add(Dense(81,activation = 'softmax'))
+        model.summary()
+        return model
